018596b97c89ccb167d8e370347293b16cb6ef95
Roy Xiang <git@royxiang.me>
2022-02-15 20:05:10 +0800

    feat: bypass caching the response for requests with Range in header

--- handler/cache.go
+++ handler/cache.go
@@ -9,7 +9,8 @@ import (
 func CacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cacheClient := r.Context().Value(cacheClientCtxKey).(*cache.Client)
-		if cacheClient != nil {
+		rangeInHeader := r.Header.Get(headerRange)
+		if cacheClient != nil && rangeInHeader == "" {
 			mu.RLock()
 			defer mu.RUnlock()
 
--- handler/const.go
+++ handler/const.go
@@ -1,11 +1,8 @@
 package handler
 
 const (
-	headerPlexPrefix     = "X-Plex-"
-	headerContainerSize  = "X-Plex-Container-Size"
-	headerContainerStart = "X-Plex-Container-Start"
-	headerToken          = "X-Plex-Token"
-	headerRange          = "Range"
+	headerToken = "X-Plex-Token"
+	headerRange = "Range"
 
 	cachePrefixDynamic = "dynamic:"
 	cachePrefixStatic  = "static:"
