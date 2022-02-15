693ddbb01effc2c0bbc59c743e76c20cad436662
Roy Xiang <git@royxiang.me>
2022-02-15 19:50:30 +0800

    feat: let dynamic router handle more requests

--- main.go
+++ main.go
@@ -22,31 +22,31 @@ func newRouter() http.Handler {
 	refreshRouter.Use(handler.RefreshMiddleware)
 	refreshRouter.Path("/library/sections/{id}/refresh").HandlerFunc(handler.Handler)
 
-	dynamicRouter := r.Methods(http.MethodGet).Subrouter()
-	dynamicRouter.Use(handler.TrafficMiddleware)
-	dynamicRouter.Use(handler.DynamicMiddleware)
-	dynamicRouter.Use(handler.CacheMiddleware)
-	dynamicRouter.PathPrefix("/hubs/").HandlerFunc(handler.Handler)
-
 	staticRouter := r.Methods(http.MethodGet).Subrouter()
 	staticRouter.Use(handler.TrafficMiddleware)
 	staticRouter.Use(handler.StaticMiddleware)
 	staticRouter.Use(handler.CacheMiddleware)
-	staticRouter.Path("/photo/:/transcode").HandlerFunc(handler.Handler)
-	staticRouter.Path("/library/metadata/{key}/art/{id}").HandlerFunc(handler.Handler)
-	staticRouter.Path("/library/metadata/{key}/thumb/{id}").HandlerFunc(handler.Handler)
+	staticRouter.
+		Path("/library/metadata/{key}/art/{id}").
+		Path("/library/metadata/{key}/thumb/{id}").
+		Path("/photo/:/transcode").
+		HandlerFunc(handler.Handler)
 
 	userRouter := r.Methods(http.MethodGet).Subrouter()
 	userRouter.Use(handler.TrafficMiddleware)
 	userRouter.Use(handler.UserMiddleware)
 	userRouter.Use(handler.CacheMiddleware)
-	userRouter.PathPrefix("/library/collections/").HandlerFunc(handler.Handler)
-	userRouter.PathPrefix("/library/metadata/").HandlerFunc(handler.Handler)
-	userRouter.PathPrefix("/library/sections/").HandlerFunc(handler.Handler)
+	userRouter.
+		PathPrefix("/library/collections/").
+		PathPrefix("/library/metadata/").
+		PathPrefix("/library/sections/").
+		HandlerFunc(handler.Handler)
 
-	getRouter := r.Methods(http.MethodGet).Subrouter()
-	getRouter.Use(handler.TrafficMiddleware)
-	getRouter.PathPrefix("/").HandlerFunc(handler.Handler)
+	dynamicRouter := r.Methods(http.MethodGet).Subrouter()
+	dynamicRouter.Use(handler.TrafficMiddleware)
+	dynamicRouter.Use(handler.DynamicMiddleware)
+	dynamicRouter.Use(handler.CacheMiddleware)
+	dynamicRouter.PathPrefix("/").HandlerFunc(handler.Handler)
 
 	r.PathPrefix("/").HandlerFunc(handler.Handler)
 	return r
